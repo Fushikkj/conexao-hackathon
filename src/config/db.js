35c51c6fa67fbd43f583920138473aebbf92636d
Fushikkj <xiguemartins@gmail.com>
2026-06-01 16:52:22 -0300

    bottando os código

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'conecta_daniel',
+    password: 'senai',
+    port: 3000
+});
+
+module.exports = pool;
--- src/routes/presencas.js
+++ src/routes/presencas.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
 
+const {
+    marcarPresenca
+} = require('../controllers/presencasController');
+
+router.post('/', marcarPresenca);
+
+module.exports = router;
